9f65378f6e052d1019c7d85903d671cb3b11277f
mkbarber <mpkbarber@gmail.com>
2017-09-18 18:46:21 -0400

    Create simple frame

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.JFrame;
+import view.MainPanel;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World");
+        
+        MainPanel mainPanel = new MainPanel();
+    }
+
+}
---
+++ src/view/MainPanel.java
@@ -0,0 +1,18 @@
+package view;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainPanel {
+    
+    public MainPanel(){
+        JFrame mainFrame = new JFrame();
+        mainFrame.setSize(800, 600);
+        mainFrame.setVisible(true);
+        mainFrame.setTitle("FIND Diagnostics Tool");
+        
+    }
+    
+}
